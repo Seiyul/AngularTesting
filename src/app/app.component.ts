e51d70ab7ad868a0ff1a0fe009c1f8ee127f1377
Seiyul <saulrodriguez.developer@gmail.com>
2021-06-11 20:55:28 +0200

    New method - watchPosition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,9 +6,13 @@ import { Component } from '@angular/core';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'Testing';
     textToPrint = '';
+
+    ngOnInit(): void {
+        // Notification.requestPermission();
+    }
 
     getCoordinates(): void {
         var options = {
@@ -18,12 +23,7 @@ export class AppComponent {
 
         const success = (pos: any) => {
             var crd = pos.coords;
-            console.log('pos -->', pos);
 
-            console.log('Your current position is:');
-            console.log('Latitude : ' + crd.latitude);
-            console.log('Longitude: ' + crd.longitude);
-            console.log('More or less ' + crd.accuracy + ' meters.');
 
             this.textToPrint = this.textToPrint +
                 '·· getCurrentPosition ··' + '\n\n' +
@@ -34,13 +34,49 @@ export class AppComponent {
                 'altitudeAccuracy: ' + crd.altitudeAccuracy + '\n' +
                 'heading: ' + crd.heading + '\n' +
                 'speed: ' + crd.speed + '\n' +
-                'timestamp: ' + new Date(pos.timestamp) + '\n\n';
+                'timestamp: ' + new Date(pos.timestamp).toISOString() + '\n' +
+                'clickDate: ' + date + '\n\n';
         };
 
-        function error(err: any) {
-            console.warn('ERROR(' + err.code + '): ' + err.message);
+        const error = (err: any) => {
+            this.textToPrint = this.textToPrint + '\n' +
+                '(!) Error ' + err.code + ': ' + err.message + '.\n';
+            console.warn('ERROR -->' + err);
         };
+        const date = new Date().toISOString();
         navigator.geolocation.getCurrentPosition(success, error, options);
+    }
+
+    // Utilización del watchPosition en lugar del getCurrentPosition
+    getCoordinates2(): void {
+        var options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        };
+
+        const success = (pos: any) => {
+            var crd = pos.coords;
+            this.textToPrint = this.textToPrint +
+                '·· watchPosition ··' + '\n\n' +
+                'latitude: ' + crd.latitude + '\n' +
+                'longitude: ' + crd.longitude + '\n' +
+                'accuracy: ' + crd.accuracy + '\n' +
+                'altitude: ' + crd.altitude + '\n' +
+                'altitudeAccuracy: ' + crd.altitudeAccuracy + '\n' +
+                'heading: ' + crd.heading + '\n' +
+                'speed: ' + crd.speed + '\n' +
+                'timestamp: ' + new Date(pos.timestamp).toISOString() + '\n' +
+                'clickDate: ' + date + '\n\n';
+
+        };
+        const error = (err: any) => {
+            this.textToPrint = this.textToPrint + '\n' +
+                '(!) Error ' + err.code + ': ' + err.message + '.\n';
+            console.warn('ERROR -->' + err);
+        };
+        const date = new Date().toISOString();
+        navigator.geolocation.watchPosition(success, error, options);
     }
 
     getFiveCoordinates(): void {
