2c309b74c5b62c2b2a89f17e5dd67d190c94a816
Seiyul <saulrodriguez.developer@gmail.com>
2021-07-07 15:42:53 +0200

    PWA button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -63,6 +63,7 @@ export class HomeComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        this.goToPwa();
         this.actualDistance = 0;
         this.lastDistance = 0;
         const params = sessionStorage.getItem('params');
@@ -264,6 +265,33 @@ export class HomeComponent implements OnInit {
     }
 
     goToPwa(): void {
-        this.textToPrint = 'Testing';
+        let deferredPrompt: any;
+        const addBtn = document.querySelector('.add-button') as HTMLElement;
+        // addBtn.style.display = 'none';
+
+        window.addEventListener('beforeinstallprompt', (e) => {
+            // Prevent Chrome 67 and earlier from automatically showing the prompt
+            e.preventDefault();
+            // Stash the event so it can be triggered later.
+            deferredPrompt = e;
+            // Update UI to notify the user they can add to home screen
+            // addBtn.style.display = 'block';
+
+            addBtn.addEventListener('click', (e) => {
+                // hide our user interface that shows our A2HS button
+                // addBtn.style.display = 'none';
+                // Show the prompt
+                deferredPrompt.prompt();
+                // Wait for the user to respond to the prompt
+                deferredPrompt.userChoice.then((choiceResult: any) => {
+                    if (choiceResult.outcome === 'accepted') {
+                        console.log('User accepted the A2HS prompt');
+                    } else {
+                        console.log('User dismissed the A2HS prompt');
+                    }
+                    deferredPrompt = null;
+                });
+            });
+        });
     }
 }
